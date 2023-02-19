247aedac51479739326e21ec43873044e25b2a50
FlRE-DEATH <48518316+FlRE-DEATH@users.noreply.github.com>
2023-02-19 14:00:13 +0200

    Add files via upload

---
+++ imports.py
@@ -0,0 +1,12 @@
+import re
+import os
+import numpy as np
+os.environ["PATH"] += os.pathsep + "F:\chromedriver"
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import requests
